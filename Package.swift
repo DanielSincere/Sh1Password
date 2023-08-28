0ad8418c08d2804fc8391d81fbbb4b80c6ec661c
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-08-28 18:21:13 -0400

    Sh 1.3 upgrade

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
   name: "Sh1Password",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
   ],
   products: [
     .library(name: "Sh1Password", targets: ["Sh1Password"])
   ],
   dependencies: [
-    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.2.0"),
+    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.3.0"),
   ],
   targets: [
     .target(name: "Sh1Password", dependencies: ["Sh"])
